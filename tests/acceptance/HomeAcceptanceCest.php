b17f20b62a1c33d652e120fd3e8ea66226dd12b6
Unknown <arno.birchler@viacesi.fr>
2018-05-25 10:42:41 +0200

    fix acceptance #16 integration #15 unit #12 test

---
+++ tests/acceptance/HomeAcceptanceCest.php
@@ -0,0 +1,21 @@
+<?php
+
+
+class HomeAcceptanceCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    public function _after(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+        $I->amOnPage('/');
+        $I->see('Parallax Template');
+        $I->see('Speeds up development');
+    }
+}
---
+++ tests/unit/HomeUnitCest.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class HomeUnitCest
+{
+    public function _before(UnitTester $I)
+    {
+    }
+
+    public function _after(UnitTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(UnitTester $I)
+    {
+        $I->assertEquals(1,1);
+    }
+}
